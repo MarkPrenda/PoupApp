3d31824201ab08db595717624040dadf8f95bbb4
Mark Prenda <markprenda.dev@gmail.com>
2026-02-22 09:46:04 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import logo from "./assets/logo.svg";
+import { Container } from "./components/Container";
+
+function App() {
+	return (
+		<>
+			<Container>
+				<aside>
+					<img src={logo} alt="" />
+					poupapp
+				</aside>
+			</Container>
+		</>
+	);
+}
+
+export default App;
